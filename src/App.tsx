5719ecbdeae9c7d3e7bcf8b91fd496dd9310ed24
Kwonkunkun <kunwoo5012@naver.com>
2021-06-02 10:49:54 +0900

    💄 add crawledList

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import "./App.css";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+import CrawledList from "./components/crawledList";
 import Header from "./components/header";
 import SearchForm from "./components/searchForm";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <SearchForm />
-        </>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Header />
+      <SearchForm />
+      <CrawledList />
+    </div>
+  );
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "60%",
+      height: "100vh",
+      margin: "auto",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+    },
+  })
+);
 
 export default App;
